230ab8948c789018e91e651d3f6e6be9f6b85dec
Afsar <afsar.s33@gmail.com>
2022-10-12 18:38:38 +0530

    Validation br (#14)
    * updated
    
    * responsive & validations
    
    * removed scroll
    
    * theme changed
    
    * form validation
    
    * confirmation page
    
    * Validations
    
    Co-authored-by: Md Major <52419744+MdMajor@users.noreply.github.com>
    Co-authored-by: MdMajor <memdmajor@gmail.com>

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
+const requiredElements = document.getElementsByClassName("isRequired");
+const formClass = document.getElementsByClassName("form-control");
+that = this;
 
-document.getElementById('submit').addEventListener('click', function() {
-  
-let name = document.getElementById("name");
+function isFormEmpty(){
+  var err = false;
+  for (let i = 0; i < requiredElements.length; i++) {
+    if(requiredElements[i].value == ""){
+      err=true;
+      requiredElements[i].classList.add("ValidationError");
+      requiredElements[i].classList.remove("ValidationSuccess");
+    }else{
+      requiredElements[i].classList.remove("ValidationError");
+      requiredElements[i].classList.add("ValidationSuccess");
+    }
+  }
+
+  return err;
+}
+
+document.getElementById('submit').addEventListener('click', function(e) {
+
+var err = that.isFormEmpty();
+if(err){
+  alert("Please Enter The Required Data");
+  return;
+}
+let username = document.getElementById("name");
 let dob = document.getElementById("dob");
 let email = document.getElementById("email");
 let phone = document.getElementById("phone");
 let city = document.getElementById("city");
 let institute = document.getElementById("institute");
-var gen = document.getElementsByName("gender");
 var qual = document.getElementsByName("c");
 
-
-
-    if (name.value == 0) {
-      window.alert("please enter your name");
-      return false;
-    }
-    else if(dob.value == 0) {
-        window.alert("please enter your date of birth");
-        return false;
-      }
-    else if(phone.value == 0) {
-        window.alert("please enter your contact number");
-        return false;
-      }
-    else if(email.value == 0) {
-        window.alert("please enter your emaili");
-        return false;
-      }
-    else{
-    
- 
-    for(let i = 0; i<gen.length; i++){
-        if(gen[i].checked){
-            var gender  = gen[i].value;
-            
-        }
-    }
     for(let i = 0; i<qual.length; i++){
         if(qual[i].checked){
             var qualification  = qual[i].value;
@@ -44,17 +40,17 @@ var qual = document.getElementsByName("c");
     }
     
     const data = JSON.stringify({
-        "name":name.value,
+        "name":username.value,
         "dob":dob.value,
         "email":email.value,
         "phone":phone.value,
         "city":city.value,
         "institute":institute.value,
         "qualification":qualification,
-        "gender":gender
       });
 
 
+      // fetch('http://localhost:9000/register',{
       fetch('https://registration-eta.vercel.app/register',{
         method: 'post',
         body:data,
@@ -62,22 +58,56 @@ var qual = document.getElementsByName("c");
             'Content-Type':'application/json'
         }
       }).then(function(response) {
-        return response.text;
+        window.location.href="/success.html";
+        // window.location.href="/success.html";
+        (function(){
+          let fields = document.getElementsByTagName("input");
+          for(let i =0;i<fields.length;i++){
+              fields[i].value="";
+          }
+        })();
+        var button = document.getElementById("submit")
+        button.value = "Update Response";
+
+        // return response.text;
+
       }).then(function(text) {
         console.log(text);
       }).catch(function(error) {
         console.log(error);
       });
-    }
+    // }
 
-      window.location.href="/success.html";
+     
+})
 
-      (function(){
-        let fields = document.getElementsByTagName("input");
-        for(let i =0;i<fields.length;i++){
-            fields[i].value="";
-        }
-      })();
-}); 
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
 
-
+function validate(oEvent){
+  var helper = {
+   "name": function(){
+    return new RegExp(/^([A-Za-z]+\s)*[a-zA-Z]+$/);
+   },
+   "phone": function(){
+    return new RegExp(/^[+0-9]*[0-9]+$/);
+   },
+   "email": function(){
+    return new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+   },
+   "institute": function(){
+    return new RegExp(/^([A-Za-z]+\s)*[a-zA-Z]+$/);
+   }
+  };
+  
+  // var element = document.getElementById(id),validation;
+  var element = oEvent.target;
+  if(!element.value.match(helper[element.id]())){
+    element.classList.add("ValidationError");
+    element.classList.remove("ValidationSuccess");
+  }else{
+    element.classList.remove("ValidationError");
+    element.classList.add("ValidationSuccess");
+  }
+}
